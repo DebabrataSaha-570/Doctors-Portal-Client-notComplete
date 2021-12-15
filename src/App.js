f41751e42adab842dde134ce85bd6321693431ff
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-12-15 21:43:47 +0600

    appbar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+
+const Home = () => {
+    return (
+        <>
+            <Navigation></Navigation>
+            <h3>This is home </h3>
+        </>
+    );
+};
+
+export default Home;

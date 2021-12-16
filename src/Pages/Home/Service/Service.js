dede872caf6e76538b6b369fb693e1407bb7807d
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-12-16 13:59:12 +0600

    card added

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-const Service = () => {
+
+const Service = (props) => {
+    const { name, img, description } = props.service;
+    const bull = (
+        <Box
+            component="span"
+            sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+        >
+            •
+        </Box>
+    );
+
     return (
-        <div>
-            <h3>This is service component</h3>
-        </div>
+        <Card sx={{ minWidth: 275 }}>
+            <CardMedia
+                component="img"
+                style={{ width: 'auto', height: '80px', margin: '0 auto' }}
+                image={img}
+                alt="Paella dish"
+            />
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    {name}
+                </Typography>
+
+                <Typography variant="body2">
+                    {description}
+
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
     );
 };
 
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -4,24 +4,45 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Container } from '@mui/material';
+import Service from '../Service/Service';
+import fluoride from '../../../images/fluoride.png'
+import cavity from '../../../images/cavity.png'
+import teeth from '../../../images/whitening.png'
+
+const services = [
+    {
+        name: 'Fluoride Treatment',
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe, qui at dicta vitae tempore repudiandae dolorem magni facilis ipsa, maiores praesentium temporibus, eum dolorum voluptates consectetur iure suscipit incidunt eveniet!',
+        img: fluoride,
+    },
+    {
+        name: 'Cavity Filling',
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe, qui at dicta vitae tempore repudiandae dolorem magni facilis ipsa, maiores praesentium temporibus, eum dolorum voluptates consectetur iure suscipit incidunt eveniet!',
+        img: cavity,
+    },
+    {
+        name: 'Teath Whitening',
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe, qui at dicta vitae tempore repudiandae dolorem magni facilis ipsa, maiores praesentium temporibus, eum dolorum voluptates consectetur iure suscipit incidunt eveniet!',
+        img: teeth,
+    },
+
+
+
+]
 
 const Services = () => {
-    const Item = styled(Paper)(({ theme }) => ({
-        ...theme.typography.body2,
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    }));
+
     return (
         <>
             <Box sx={{ flexGrow: 1 }}>
                 <Container >
                     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                        {Array.from(Array(6)).map((_, index) => (
-                            <Grid item xs={4} sm={4} md={4} key={index}>
-                                <Item>xs=2</Item>
+                        {services.map(service =>
+                            <Grid item xs={4} sm={4} md={4} key={service.name}>
+                                <Service service={service}></Service>
+
                             </Grid>
-                        ))}
+                        )}
                     </Grid>
                 </Container>
             </Box>

14b8eb70ff7c5acda6a3ab619439f58adc260001
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-12-17 00:19:34 +0600

    background image and image pop up added

--- src/Pages/Home/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Home/AppointmentBanner/AppointmentBanner.js
@@ -5,11 +5,15 @@ import doctor from '../../../images/doctor.png'
 import appoinment from '../../../images/appointment-bg.png'
 
 import Grid from '@mui/material/Grid';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 
 
 const appinmentBg = {
-    background: `url(${appoinment})`
+    background: `url(${appoinment})`,
+    marginTop: 150,
+    backgroundColor: 'rgb(23, 35, 64, 0.8 )',
+    // backgroundColor: 'rgba(83, 93, 118, 0.8)',
+    backgroundBlendMode: 'darken, luminosity'
 }
 
 
@@ -18,12 +22,22 @@ const AppointmentBanner = () => {
         <Box style={appinmentBg} sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
                 <Grid item xs={12} md={6}>
-                    <img style={{ width: '400px' }} src={doctor} alt="" />
+                    <img style={{ width: '400px', marginTop: "-110px" }} src={doctor} alt="" />
                 </Grid>
-                <Grid item xs={12} md={6}>
-                    <Typography variant="h6" >
-                        APPOINMENT
-                    </Typography>
+                <Grid item xs={12} md={6}
+                    sx={{ display: 'flex', justifyContent: 'flex-start', textAlign: 'left', alignItems: 'center' }}>
+                    <Box>
+                        <Typography variant="h6" sx={{ mb: 5 }} style={{ color: '#5CE7ED' }} >
+                            Appoinment
+                        </Typography>
+                        <Typography variant="h4" style={{ color: 'white' }} >
+                            Make an appointment today
+                        </Typography>
+                        <Typography variant="h6" sx={{ my: 5 }} style={{ color: 'white', fontSize: 14, fontWeight: 400 }} >
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit blanditiis corrupti totam vel iure dicta nulla atque accusantium sint rem?
+                        </Typography>
+                        <Button style={{ backgroundColor: '#5CE7ED' }} variant="contained">Lean more</Button>
+                    </Box>
                 </Grid>
 
             </Grid>

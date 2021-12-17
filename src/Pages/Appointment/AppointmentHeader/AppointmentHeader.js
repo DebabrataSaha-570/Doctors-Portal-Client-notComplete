c49eaa4f982b6e05aa57ecb40517c63dff5307ba
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-12-18 02:21:17 +0600

    created appointment header with mui date picker

--- src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -1,10 +1,20 @@
+import { Container, Grid } from '@mui/material';
 import React from 'react';
-
+import chair from '../../../images/chair.png'
+import Calender from '../../Shared/Calender/Calender';
 const AppointmentHeader = () => {
     return (
-        <div>
-            <h3>This is appointment header</h3>
-        </div>
+        <Container>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                    <Calender></Calender>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <img src={chair} style={{ width: '100%' }} alt="" />
+                </Grid>
+
+            </Grid>
+        </Container>
     );
 };
 

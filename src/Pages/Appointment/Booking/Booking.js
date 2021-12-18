5b140e8e1f1fc9069196b8acb1ef218d6aa0b0d7
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-12-19 01:56:36 +0600

    displayed a modal from on booking click with dynamic data

--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -4,24 +4,32 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import { Typography } from '@mui/material';
 import Button from '@mui/material/Button';
+import BookingModal from '../BookingModal/BookingModal';
 const Booking = ({ booking }) => {
     const { id, name, time, space } = booking
-    return (
-        <Grid item xs={12} sm={2} md={4}>
-            <Paper elevation={3} sx={{ py: 5 }}>
-                <Typography sx={{ color: 'info.main', fontWeight: 600 }} variant="h5" gutterBottom component="div">
-                    {name}
-                </Typography>
-                <Typography variant="h6" gutterBottom component="div">
-                    {time}
-                </Typography>
-                <Typography variant="caption" display="block" gutterBottom>
-                    {space} SPACES AVAILABLE
-                </Typography>
-                <Button variant="contained">BOOK APPOINTMENT</Button>
-            </Paper>
 
-        </Grid>
+    const [openBooking, setBookingOpen] = React.useState(false);
+    const handleBookingOpen = () => setBookingOpen(true);
+    const handleBookingClose = () => setBookingOpen(false);
+    return (
+        <>
+            <Grid item xs={12} sm={2} md={4}>
+                <Paper elevation={3} sx={{ py: 5 }}>
+                    <Typography sx={{ color: 'info.main', fontWeight: 600 }} variant="h5" gutterBottom component="div">
+                        {name}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom component="div">
+                        {time}
+                    </Typography>
+                    <Typography variant="caption" display="block" gutterBottom>
+                        {space} SPACES AVAILABLE
+                    </Typography>
+                    <Button onClick={handleBookingOpen} variant="contained">BOOK APPOINTMENT</Button>
+                </Paper>
+
+            </Grid>
+            <BookingModal booking={booking} openBooking={openBooking} handleBookingClose={handleBookingClose}></BookingModal>
+        </>
     );
 };
 
--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
-const BookingModal = () => {
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+const BookingModal = ({ openBooking, handleBookingClose, booking }) => {
+    const { name, time } = booking
+
     return (
-        <div>
-            <h3>This is booking modal</h3>
-        </div>
+        <Modal
+            open={openBooking}
+            onClose={handleBookingClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    {name}
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    {time}
+                </Typography>
+            </Box>
+        </Modal>
     );
 };
 
